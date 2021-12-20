63ee983b2eee147299faf0fe54c2d8a4da17eb62
vasugaur1998 <vasugaur98@gmail.com>
2021-12-20 20:21:24 +0530

    updated

---
+++ function.js
@@ -0,0 +1,57 @@
+// key Points of functions
+
+// 1. return kuch bhi hosakta hai so  we dont have to write the return type
+// 2. if kuch return ni hua to undefined return hoga. if agr main value save karlu to
+
+
+
+
+// function fn(a,b){
+//     console.log("hello a", a);
+//     console.log("hello b",b);
+
+// }
+
+// fn(10,20);
+
+
+// output--->
+// hello a 10
+// hello b 20
+
+
+// ---------------------------------------------------------------------
+// function fn(a,b){
+//     console.log("hello a", a);
+//     console.log("hello b",b);
+//    return 10;
+// }
+
+// let value=fn(10,20);
+// console.log(value);
+
+
+// output--->
+// hello a 10
+// hello b 20
+// 10
+// ---------------------------------------------------------------------------
+
+
+
+// function fn(a,b){
+//         console.log("hello a", a);
+//         console.log("hello b",b);
+       
+//     }
+    
+//     let value=fn(10,20);
+//     console.log(value);
+    
+
+
+//   output--->
+//   hello a 10
+// hello b 20
+// undefined
+  
---
+++ typeOf.js
@@ -0,0 +1,24 @@
+// let a=10;
+// console.log(typeof a);
+// a="hdhdh";
+// console.log(typeof a);
+// a=true;
+// console.log(typeof a);
+// a=null;
+// console.log(typeof a);
+
+
+// TYPE OF--> "tells us about the type of the variable"
+
+
+
+// input-->manage
+
+// String to Array :- Because input of array ek string me ata hai so to convert it into array of numbers
+
+let str="[1,2,3,4,5]";
+console.log(str,typeof str);
+let arr=JSON.parse(str);
+console.log(arr,Array.isArray(arr));
+
+
